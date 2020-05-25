93fa20e476ccad86241589b2873dd3f495f9836a
Tadej Vengust <vengust.tadej@gmail.com>
2020-05-25 09:04:17 +0200

    Fix build.

--- packages/rawmodel-parsers/.eslintrc.js
+++ packages/rawmodel-parsers/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "project": "./tsconfig.json",
         "sourceType": "module",
         "tsconfigRootDir": __dirname,
     },
